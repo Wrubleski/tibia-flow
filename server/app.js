6b1212a2c3517d7f328eff095d8909b598325757
Wrubleski <henrike.hip@live.com>
2021-01-20 18:51:33 -0300

    Separando o backend do frontend

--- server/app.js
+++ server/app.js
@@ -1,25 +1,14 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-const cors = require("cors");
-const bodyParser = require("body-parser");
 
 app.use(
   "/static",
   express.static(path.join(__dirname, "../client/build/static"))
 );
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(cors());
-
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
-
-app.post("/loot", (req, res) => {
-  res.send(true);
 });
 
 app.listen(process.env.PORT || 3001, () => {
