cc81bbf2c514d64755000ec361e8433f0c822e29
Wrubleski <henrike.hip@live.com>
2021-01-20 15:58:56 -0300

    retirada a build. Commit teste pro heroku.

--- server/app.js
+++ server/app.js
@@ -1,16 +1,27 @@
 const path = require("path");
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 app.use(
   "/static",
   express.static(path.join(__dirname, "../client/build/static"))
 );
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.post("/loot", (req, res) => {
+  res.send(true);
+});
+
+app.listen(process.env.PORT || 3001, () => {
   console.log("Server is up and running.");
 });
