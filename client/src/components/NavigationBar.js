81bd4674b79ae62d8b1a5530f1bb9f11b17d7c6c
Wrubleski <henrike.hip@live.com>
2021-02-23 20:28:29 -0300

    fix navbar link re rendering the whole page

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -1,13 +1,21 @@
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import { LinkContainer } from "react-router-bootstrap";
 
 const NavigationBar = () => {
   return (
     <Navbar bg="dark" variant="dark" className="mb-3">
-      <Navbar.Brand href="/">Tibia Flow</Navbar.Brand>
+      <LinkContainer to="/">
+        <Navbar.Brand>Tibia Flow</Navbar.Brand>
+      </LinkContainer>
       <Nav className="ml-auto">
-        <Nav.Link href="/login">Login</Nav.Link>
-        <Nav.Link href="/register">Register</Nav.Link>
+        <LinkContainer to="/login">
+          <Nav.Link>Login</Nav.Link>
+        </LinkContainer>
+
+        <LinkContainer to="/register">
+          <Nav.Link>Register</Nav.Link>
+        </LinkContainer>
       </Nav>
     </Navbar>
   );
